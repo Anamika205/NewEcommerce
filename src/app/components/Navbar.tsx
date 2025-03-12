dea64feb343df440ac2ee1336a8fb836de5e2dfc
Anamika <anamikahajipur2000@gmail.com>
2025-03-12 10:43:11 +0530

    resolving error which comes

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -8,9 +8,7 @@ const Navbar = () => {
           <Link className="navbar_link relative" href="/">HOME</Link>
           <Link className="navbar_link relative" href="/mens">MEN&apos;S</Link>
           <Link className="navbar_link relative" href="/womens">WOMEN&apos;S</Link>
-          {/* <Link className="navbar_link relative" href="/categories">CATEGORIES</Link> */}
-          {/* <Link className="navbar_link relative" href="/offers">OFFER&apos;S</Link>
-                <Link className="navbar_link relative" href="/blogs">BLOGS</Link> */}
+         
         </div>
       </div>
     </div>
